8921ff7c4c9ba22a5b4fd1d1e3a6a63bdad8f4a1
Karimibr <s129199@ap.be>
2023-11-10 15:53:05 +0100

    walking sprite animation working

--- GameProject/GameProject/Game1.cs
+++ GameProject/GameProject/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct3D9;
+using SamplerState = Microsoft.Xna.Framework.Graphics.SamplerState;
 
 namespace GameProject
 {
@@ -8,6 +10,11 @@ namespace GameProject
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Rectangle _deelRectangle;
+        private Texture2D _texture;
+        private int schuifOp_X = 10;
+
+
 
         public Game1()
         {
@@ -20,7 +27,10 @@ namespace GameProject
         {
             // TODO: Add your initialization logic here
 
+            _deelRectangle = new Rectangle(schuifOp_X, 0, 35, 32);
             base.Initialize();
+
+
         }
 
         protected override void LoadContent()
@@ -28,6 +38,7 @@ namespace GameProject
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _texture = Content.Load<Texture2D>("Walk-Anim");
         }
 
         protected override void Update(GameTime gameTime)
@@ -43,10 +54,22 @@ namespace GameProject
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            float scale = 5f;
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            _spriteBatch.Draw(_texture, new Vector2(0, 0), _deelRectangle, Color.White,0f, new Vector2(0, 0), scale,0,0);
+            _spriteBatch.End();
 
-            // TODO: Add your drawing code here
+            schuifOp_X += 48;
+            if (schuifOp_X > 180)
+            {
+                schuifOp_X = 10;
+            }
+            _deelRectangle.X = schuifOp_X;
 
             base.Draw(gameTime);
+            
+
+
         }
     }
 }
